7eff95ef06a363ba195596b656ef9bf0be1c753a
Alejandro2097 <alejo12381@hotmail.es>
2023-03-28 22:09:15 -0500

    saving

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+// variables y constantes
+
+const nombre = 'Fernando';
+let apellido = 'Herrera';
+
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+if(true) {
+    const nombre = 'Peter';
+    let valorDado = 6;
+    console.log(valorDado);
+}
+
+console.log(valorDado);
