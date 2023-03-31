d30130651a25007fde9adeb87bcc08b7fb0aceed
Alejandro2097 <alejo12381@hotmail.es>
2023-03-30 20:25:44 -0500

    solution

--- src/index.js
+++ src/index.js
@@ -15,11 +15,24 @@ console.log(saludar2('Goku'));
 console.log(saludar3('Vegueta'));
 console.log(saludar4());
 
-const getUser = () => {
-    return {
+const getUser = () => (
+     {
         uid: 'ABC123',
         username: 'El_papu1231'
     }
-}
+);
+const user = getUser();
+console.log(user);
 
-console.log(getUser())
+//Tarea
+// 1. funcion de flecha
+// 2. Tiene que retornar un objeto implicito 
+// 3. Pruebas
+const getUsuarioActivo = (nombre) => ( {
+        uid: 'ABC567',
+        username: nombre
+} )
+
+const usuarioActivo = getUsuarioActivo('Alejandro');
+
+console.log(usuarioActivo);
