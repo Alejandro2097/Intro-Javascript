ebbb80f15a604d4811b5451651d3ceb0fa481bbe
Alejandro2097 <alejo12381@hotmail.es>
2023-03-30 20:04:58 -0500

    saludar2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@
 const saludar = function(nombre) {
     return `Hola, ${nombre}`;
 }
+const saludar2 = (nombre) => {
+    return `Hola, ${nombre}`;
+}
 
-
-console.log(saludar);
+const saludar3 = (nombre) => {
+    return `Hola, ${nombre}`;
+}
+console.log(saludar2);
