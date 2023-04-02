bb1970cd4eec0df321d12f6254cd94b2c7d9bef3
Alejandro2097 <alejo12381@hotmail.es>
2023-04-01 23:47:25 -0500

    start class

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+
+// Desestructuracion
+// Asignacion Desestructurante
+
+const persona = {
+    nombre: 'Tony',
+    edad: 45,
+    clave: 'Ironman'
+}
+
+
+const {nombre, edad, clave} = persona;
+
+console.log(nombre);
+console.log(edad);
+console.log(clave);
