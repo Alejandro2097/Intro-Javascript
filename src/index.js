016003e650530f527a402868f30c075725b3457f
Alejandro2097 <alejo12381@hotmail.es>
2023-04-02 22:44:27 -0500

    reject

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import { getHeroesById } from "./bases/08-im-exp";
 
 
-const promesa = new Promise((resolve, reject) => {
+// const promesa = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+
+//         // Tarea 
+//         // importen el
+//         const heroe = getHeroesById(2);
+//         resolve(heroe);
+//         // resolve();
+//     }, 2000);
+    
+// });
+
+
+// promesa.then((heroe) => {
+//     console.log('Heroe', heroe)
+// }).catch(err => console.warn(err));
+
+const getHeroeByIdAsync = (id) => {
+    return new Promise((resolve, reject) => {
     setTimeout(() => {
 
         // Tarea 
         // importen el
-        const heroe = getHeroesById(2);
+        const heroe = getHeroesById(id);
         resolve(heroe);
         // resolve();
     }, 2000);
     
-});
+    });
 
+}
 
-promesa.then((heroe) => {
-    console.log('Heroe', heroe)
-}).catch(err => console.warn(err));
+getHeroeByIdAsync(4).then(heroe => {
+    console.log('Heroe', heroe);
+})
+
