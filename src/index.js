43d6780a7c3887f59432e4a46c055c7e42d144eb
Alejandro2097 <alejo12381@hotmail.es>
2023-04-02 21:51:07 -0500

    home work

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { heroes } from './data/heroes';
 
 
-console.log(heroes);
+const getHeroesById = (id) => {
+    return heroes.find((heroe) => heroe.id === id)
+}
+
+console.log(getHeroesById(2));
