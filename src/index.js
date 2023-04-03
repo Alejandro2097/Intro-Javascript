8380c24f05bed6f8720f02b11de084d083df4aeb
Alejandro2097 <alejo12381@hotmail.es>
2023-04-02 22:14:47 -0500

    promise

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+
+const promesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve();
+    }, 2000);
+    
+});
+
+
+promesa.then(() => {
+    console.log('Then de la promesa')
+})
